9ff9e3bdc617e27015a94cfc96012085108c2470
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-06-27 11:54:04 +0100

    ...

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,34 +4,32 @@ const jwt = require('jsonwebtoken')
 
 
 const verifyAuth = async (req, res) => {
-    const { username, password } = req.body
-    if (!username || !password)
+    const { user, pwrd } = req.body
+    if (!user || !pwrd)
         return res.status(400).json({ 'message': 'username and password are required' })
     
-    const foundUser = await Users.findOne({ username: username}).exec()   
+    const foundUser = await Users.findOne({ username: user}).exec()   
     if (!foundUser)
         return res.sendStatus(403)
     
-    const matchedUser = await bcrypt.compare(password, foundUser.password)
+    const matchedUser = await bcrypt.compare(pwrd, foundUser.password)
 
     if (matchedUser) {
         const accessToken = jwt.sign(
-            { "username": foundUser.username },
+            { "user": foundUser.username },
             process.env.ACCESS_TOKEN_SECRET,
             { expiresIn: '30s' }
         );
         const refreshToken = jwt.sign(
-            { "username": foundUser.username },
+            { "user": foundUser.username },
             process.env.REFRESH_TOKEN_SECRET,
             { expiresIn: '1d' }
         );
         foundUser.refreshToken = refreshToken
         const userAuth = await foundUser.save()
-        console.log(userAuth);
 
-        res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 })
+        res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 })
         res.json(accessToken);
-        console.log(accessToken);
     }
     else {
         res.sendStatus(401)
--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -2,10 +2,9 @@ const jwt = require('jsonwebtoken')
 
 const verifyJWT = (req, res, next) => {
     const authHeaders = req.headers.authorization || req.headers.Authorization
-    if (!authHeaders?.startswith('Bearer'))
+    if (!authHeaders || !authHeaders?.startsWith('Bearer'))
         return res.sendStatus(401)
-    const token = authHeaders.split('')[1]
-    console.log(token);
+    const token = authHeaders.split(' ')[1]
 
     jwt.verify(
         token,
@@ -13,7 +12,7 @@ const verifyJWT = (req, res, next) => {
         (err, decoded) => {
             if (err)
                 return res.sendStatus(403)
-            req.username = decoded.username
+            req.user = decoded.username
             next();
         }
     );
