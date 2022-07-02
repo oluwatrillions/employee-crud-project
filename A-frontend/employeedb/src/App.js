3b73c46527b13a94208d56126a309f826e63d31e
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-07-02 17:59:20 +0100

    sign in page

--- A-frontend/employeedb/src/App.js
+++ A-frontend/employeedb/src/App.js
@@ -3,9 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-          <header className="App-header">
-              Welcome home
-      </header>
+            <section className="App-section">
+              <img src="./images/profile.png" alt="" />
+              <div className="container">
+                  <div className="input">
+                      <div className="imageBox">
+                         <img src="./images/pp.png" alt="" /> 
+                      </div>
+                      <input type="text" name="email" placeholder="Email/ID"/>
+                  </div>
+                  <div className="input">
+                      <div className="imageBox">
+                         <img src="./images/password.png" alt="" /> 
+                      </div>
+                      <input type="password" name="password" placeholder="Password"/>
+                  </div>
+              </div>
+              <button>LOGIN</button>
+        </section>
     </div>
   );
 }
